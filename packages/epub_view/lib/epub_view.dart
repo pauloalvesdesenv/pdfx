43eeb7b9d52fcbcb2ab3de20b5935fa43fc0c6c3
Paulo Ricardo <paulo.alves.desenv@gmail.com>
2025-04-29 08:30:30 -0300

    wip

--- packages/epub_view/lib/epub_view.dart
+++
@@ -1,6 +0,0 @@
-export 'package:epubx/epubx.dart';
-
-export 'src/ui/actual_chapter.dart';
-export 'src/ui/epub_view.dart';
-export 'src/ui/table_of_contents.dart';
-export 'src/helpers/epub_document.dart';
